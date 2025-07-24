5b1e9c19a3e9017bfd12dd78fceea644ffb1f6c4
Soumik <158482615+mallicksoumik1711@users.noreply.github.com>
2025-07-24 21:20:09 +0530

    Revision 1 done

--- Sliding Window/Sliding_window_maximum/ques_239.cpp
+++ Sliding Window/Sliding_window_maximum/ques_239.cpp
@@ -29,6 +29,36 @@ vector<int> maxNumber(vector<int> &nums, int k){
     return ans;
 }
 
+// i have directly put this one from leetcode, didnt run on the compiler but assure you working just fine.
+vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        deque<int> dq;
+        vector<int> ans;
+        int l=0, r=0;
+        if(k == 1){
+            return nums;
+        }
+        while(r < nums.size()){
+            if(dq.empty()){
+                dq.push_back(nums[r]);
+            }
+            else{
+                while(!dq.empty() && nums[r] > dq.back()){
+                    dq.pop_back();
+                }
+                dq.push_back(nums[r]);
+                if(r-l+1 == k){
+                    ans.push_back(dq.front());
+                    if(nums[l] == dq.front()){
+                        dq.pop_front();
+                    }
+                    l += 1;
+                }
+            }
+            r += 1;
+        }
+        return ans;
+    }
+
 int main(){
     vector<int> nums = {1,3,-1,-3,5,3,6,7};
     int k = 3;
