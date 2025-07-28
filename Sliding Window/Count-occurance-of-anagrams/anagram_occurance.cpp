b693626e5058e6f126e392923f165b8d52e1bae3
Soumik <158482615+mallicksoumik1711@users.noreply.github.com>
2025-07-28 13:34:52 +0530

    Revision 1

--- Sliding Window/Count-occurance-of-anagrams/anagram_occurance.cpp
+++ Sliding Window/Count-occurance-of-anagrams/anagram_occurance.cpp
@@ -3,6 +3,38 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+vector<int> findAnagrams(string s, string p) {
+        vector<int> idx;
+        unordered_map<char, int> mpp;
+        for(int i=0; i<p.size(); i++){
+            mpp[p[i]] += 1;
+        }
+        int count = mpp.size();
+        int l=0, r=0;
+        while(r < s.size()){
+            if(mpp.find(s[r]) != mpp.end()){
+                mpp[s[r]] -= 1;
+                if(mpp[s[r]] == 0){
+                    count -= 1;
+                }
+            }
+            while(r-l+1 == p.size()){
+                if(count == 0){
+                    idx.push_back(l);
+                }
+                if(mpp.find(s[l]) != mpp.end()){
+                    if(mpp[s[l]] == 0){
+                        count += 1;
+                    }
+                    mpp[s[l]] += 1;
+                }
+                l += 1;
+            }
+            r += 1;
+        }
+        return idx;
+    }
+
 int countOccurance(string &s, string &t){
     unordered_map<char, int> mpp;
     for(int i=0; i<t.size(); i++){
