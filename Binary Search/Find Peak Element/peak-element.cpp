bdf7504bb9ae5f43cacb37ec1e896c7fa433b175
Soumik <158482615+mallicksoumik1711@users.noreply.github.com>
2025-08-22 11:48:13 +0530

    Revision 1

--- Binary Search/Find Peak Element/peak-element.cpp
+++ Binary Search/Find Peak Element/peak-element.cpp
@@ -17,7 +17,7 @@ int peakElement(vector<int> &nums){
     while(l <= h){
         int mid = (l+h)/2;
         if(nums[mid] > nums[mid-1] && nums[mid] > nums[mid+1]){
-            return mid;                                                     //returning index not value
+            return mid;                                                     //returning INDEX not VALUE
         }
         else if(nums[mid] > nums[mid-1] && nums[mid] < nums[mid+1]){
             l = mid+1;
