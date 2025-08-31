84415842781ea29dd3c9c84df777868ca27e5e87
Soumik <158482615+mallicksoumik1711@users.noreply.github.com>
2025-08-31 13:28:46 +0530

    Revision 1

--- Two Pointer/Array modification in place/Remove elements/remove_elements.cpp
+++ Two Pointer/Array modification in place/Remove elements/remove_elements.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 /*
 Example 1:
-
+   
 Input: nums = [3,2,2,3], val = 3
 Output: 2, nums = [2,2,_,_]
 Explanation: Your function should return k = 2, with the first two elements of nums being 2.
