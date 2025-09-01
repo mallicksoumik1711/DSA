9194ef0232a5c97f4ab5e33aeac24bdffd66fddb
Soumik <158482615+mallicksoumik1711@users.noreply.github.com>
2025-09-01 21:17:59 +0530

    Revision 1

--- Sliding Window/Subarray-with-k-different-integers/k-unique-subarrays.cpp
+++ Sliding Window/Subarray-with-k-different-integers/k-unique-subarrays.cpp
@@ -9,7 +9,7 @@ Explanation: Subarrays formed with exactly 2 different integers: [1,2], [2,1], [
 Example 2:
 
 Input: nums = [1,2,1,3,4], k = 3
-Output: 3
+Output: 3    
 Explanation: Subarrays formed with exactly 3 different integers: [1,2,1,3], [2,1,3], [1,3,4].
 */
 
