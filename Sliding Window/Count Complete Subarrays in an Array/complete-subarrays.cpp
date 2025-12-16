ca115ca89fe6be61ada3f08c95f263c3a5cd963c
Soumik <158482615+mallicksoumik1711@users.noreply.github.com>
2025-12-16 20:33:56 +0530

    Refactor complete-subarrays.cpp for clarity
    Removed commented-out code and cleaned up logic.

--- Sliding Window/Count Complete Subarrays in an Array/complete-subarrays.cpp
+++ Sliding Window/Count Complete Subarrays in an Array/complete-subarrays.cpp
@@ -28,8 +28,7 @@ int countSubarrays(vector<int> &nums, int n){
                 mpp.erase(nums[l]);
             }
             l += 1;
-            // if mpp[nums[i] is > 0
-            // then also this mught work
+            
         }
         count += r-l+1;
         r += 1;
