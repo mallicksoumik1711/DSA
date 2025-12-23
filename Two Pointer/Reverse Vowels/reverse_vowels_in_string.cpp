bfeb1690a1f053fa351ac9566de2ad021d5fcf98
Soumik <158482615+mallicksoumik1711@users.noreply.github.com>
2025-12-23 20:44:22 +0530

    Implement isVowel function in reverse_vowels_in_string.cpp
    Add a function to check if a character is a vowel.

--- Two Pointer/Reverse Vowels/reverse_vowels_in_string.cpp
+++ Two Pointer/Reverse Vowels/reverse_vowels_in_string.cpp
@@ -21,6 +21,8 @@ Input: s = "leetcode"
 Output: "leotcede"
 */
 
+//used two pointers, we can also use hash maps
+
 bool isVowel(char ch){
         if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'||ch=='A'||ch=='E'||ch=='I'||ch=='O'||ch=='U'){
             return true;
